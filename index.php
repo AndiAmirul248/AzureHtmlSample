cfb2a0d08391e30f7ec39e8b2ff7ac3803986e25
AndiAmirul248 <33323950+AndiAmirul248@users.noreply.github.com>
2019-05-21 12:49:53 +0800

    Update index.php

--- index.php
+++ index.php
@@ -28,16 +28,14 @@
  <?php
     $host = "codingatthenight.database.windows.net";
     $user = "codingatthenight";
-    $pass = "Hacker721";
+    $pass = "hacker721";
     $db = "dbCodingAtTheNight";
-
     try {
         $conn = new PDO("sqlsrv:server = $host; Database = $db", $user, $pass);
         $conn->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
     } catch(Exception $e) {
         echo "Failed: " . $e;
-    }
-
+    } 
     if (isset($_POST['submit'])) {
         try {
             $name = $_POST['name'];
@@ -45,7 +43,7 @@
             $job = $_POST['job'];
             $date = date("Y-m-d");
             // Insert data
-            $sql_insert = "INSERT INTO Registration (name, email, job, date) 
+            $sql_insert = "INSERT INTO Registration (nama, email, pekerjaan, tgl) 
                         VALUES (?,?,?,?)";
             $stmt = $conn->prepare($sql_insert);
             $stmt->bindValue(1, $name);
@@ -56,11 +54,10 @@
         } catch(Exception $e) {
             echo "Failed: " . $e;
         }
-
         echo "<h3>Your're registered!</h3>";
     } else if (isset($_POST['load_data'])) {
         try {
-            $sql_select = "SELECT * FROM Registration";
+            $sql_select = "SELECT * FROM registration";
             $stmt = $conn->query($sql_select);
             $registrants = $stmt->fetchAll(); 
             if(count($registrants) > 0) {
@@ -71,10 +68,10 @@
                 echo "<th>Job</th>";
                 echo "<th>Date</th></tr>";
                 foreach($registrants as $registrant) {
-                    echo "<tr><td>".$registrant['name']."</td>";
+                    echo "<tr><td>".$registrant['nama']."</td>";
                     echo "<td>".$registrant['email']."</td>";
-                    echo "<td>".$registrant['job']."</td>";
-                    echo "<td>".$registrant['date']."</td></tr>";
+                    echo "<td>".$registrant['pekerjaan']."</td>";
+                    echo "<td>".$registrant['tgl']."</td></tr>";
                 }
                 echo "</table>";
             } else {
